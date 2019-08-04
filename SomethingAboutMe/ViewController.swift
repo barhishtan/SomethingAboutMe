cdfbcc7bf35510ee7e4de6b392cd58567225daba
Artur Sokolov <barhishtan@gmail.com>
2019-08-04 14:59:22 +0200

    Initial Commit

---
+++ SomethingAboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SomethingAboutMe
+//
+//  Created by Artur Sokolov on 04/08/2019.
+//  Copyright © 2019 Artur Sokolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
