224fa9a6e868e0253f0ab9aec73d69cb68bf52e2
Artur Sokolov <barhishtan@gmail.com>
2019-08-05 10:43:07 +0200

    12. small change

---
+++ SomethingAboutMe/Configure.swift
@@ -0,0 +1,15 @@
+//
+//  Configure.swift
+//  SomethingAboutMe
+//
+//  Created by Artur Sokolov on 04/08/2019.
+//  Copyright © 2019 Artur Sokolov. All rights reserved.
+//
+
+import UIKit
+
+func configurePhoto(_ photo: UIImageView) {
+    photo.layer.cornerRadius = 10
+    photo.layer.borderWidth = 5
+    photo.layer.borderColor = UIColor.white.cgColor
+}
